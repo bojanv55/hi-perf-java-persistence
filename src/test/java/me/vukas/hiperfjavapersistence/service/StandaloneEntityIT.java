dab7b8f13016e61bbfac1039b25d0a9e87e4af81
default <bojanv55@gmail.com>
2019-06-03 22:14:50 +0200

    - application-default.yml loads when tests are run and merges+overwrites application.yml. I could use application.yml in tests, but this would completely overwrite application.yml from main. - added docker-compose.yml to run mysql server in manual mode (for manual testing of main classes).

--- src/test/java/me/vukas/hiperfjavapersistence/service/StandaloneEntityIT.java
+++ src/test/java/me/vukas/hiperfjavapersistence/service/StandaloneEntityIT.java
@@ -1,12 +1,10 @@
 package me.vukas.hiperfjavapersistence.service;
 
-import lombok.AllArgsConstructor;
 import me.vukas.hiperfjavapersistence.entity.StandaloneEntity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -15,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
 public class StandaloneEntityIT {
 
     @Autowired
