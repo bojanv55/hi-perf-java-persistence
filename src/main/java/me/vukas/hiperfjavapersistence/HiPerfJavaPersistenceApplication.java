f119073c6978913b27011024bd64017242f84bfd
default <bojanv55@gmail.com>
2019-06-03 19:02:27 +0200

    first commit

---
+++ src/main/java/me/vukas/hiperfjavapersistence/HiPerfJavaPersistenceApplication.java
@@ -0,0 +1,13 @@
+package me.vukas.hiperfjavapersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiPerfJavaPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiPerfJavaPersistenceApplication.class, args);
+	}
+
+}
