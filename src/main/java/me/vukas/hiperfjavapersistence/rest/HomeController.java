2f0c60ae86ebeddef52102462854994eb28ce87f
default <bojanv55@gmail.com>
2019-06-04 20:16:52 +0200

    return entity from controller

--- src/main/java/me/vukas/hiperfjavapersistence/rest/HomeController.java
+++ src/main/java/me/vukas/hiperfjavapersistence/rest/HomeController.java
@@ -1,14 +1,36 @@
 package me.vukas.hiperfjavapersistence.rest;
 
+import lombok.AllArgsConstructor;
+import me.vukas.hiperfjavapersistence.entity.StandaloneEntity;
+import me.vukas.hiperfjavapersistence.service.StandaloneEntityService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
+@AllArgsConstructor
 public class HomeController {
 
-  @GetMapping("some")
-  public String getSome(){
-    return "Some";
+  private StandaloneEntityService standaloneEntityService;
+
+  @GetMapping("entity/{id}")
+  public StandaloneEntity readEntity(@PathVariable Integer id){
+    return standaloneEntityService.getStandaloneEntity(id)
+            .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Unable to find resource"));
+  }
+
+  @GetMapping("entity/insert/{id}") //should be POST
+  @ResponseStatus(HttpStatus.CREATED)
+  public void addEntity(@PathVariable Integer id){
+    StandaloneEntity standaloneEntity = new StandaloneEntity();
+    standaloneEntity.setId(id);
+    standaloneEntity.setName("NameFor" + id);
+    standaloneEntityService.saveStandaloneEntity(standaloneEntity);
   }
 
 }
