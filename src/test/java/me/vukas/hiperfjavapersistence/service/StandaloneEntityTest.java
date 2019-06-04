90e184371905efa5b54e41242d45331045c66031
Bojan Vukasovic <b.vukasovic@sportradar.com>
2019-06-04 13:14:04 +0200

    use failsafe for *IT integration tests and surefire for *Test unit test classes.

---
+++ src/test/java/me/vukas/hiperfjavapersistence/service/StandaloneEntityTest.java
@@ -0,0 +1,17 @@
+package me.vukas.hiperfjavapersistence.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class StandaloneEntityTest {
+
+  @Test
+  public void someUnitTestShouldWork(){
+    assertThat(true).isTrue();
+  }
+
+}
