adacd930f21a40b9ea52851de31c578a0514dabe
default <bojanv55@gmail.com>
2019-06-15 13:43:36 +0200

    added @Embeddable to be consistent with @ElementCollections embeddable

--- src/main/java/me/vukas/hiperfjavapersistence/entity/relationship/onetomany/CombinationOfAttribs.java
+++ src/main/java/me/vukas/hiperfjavapersistence/entity/relationship/onetomany/CombinationOfAttribs.java
@@ -5,9 +5,12 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
+import javax.persistence.Embeddable;
+
 @Value
 @RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Embeddable //not necessary, but to be consistent with @ElementCollection where it has to be @Embeddable
 public class CombinationOfAttribs {
   private String attrib1;
   private Integer attrib2;
--- src/main/java/me/vukas/hiperfjavapersistence/entity/relationship/onetomany/CompositeId.java
+++ src/main/java/me/vukas/hiperfjavapersistence/entity/relationship/onetomany/CompositeId.java
@@ -5,11 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
+import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Value
 @RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Embeddable //not necessary, but to be consistent with @ElementCollection where it has to be @Embeddable
 public class CompositeId implements Serializable {
     private final Long id1;
     private final Long id2;
