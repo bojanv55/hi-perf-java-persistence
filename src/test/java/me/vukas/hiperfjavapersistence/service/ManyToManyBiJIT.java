3d9176bd2146255b2a46ead06af2cf2a94609673
default <bojanv55@gmail.com>
2019-06-19 23:52:50 +0200

    failing many to many with join table when saving all at once

--- src/test/java/me/vukas/hiperfjavapersistence/service/ManyToManyBiJIT.java
+++ src/test/java/me/vukas/hiperfjavapersistence/service/ManyToManyBiJIT.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ManyToManyBiJIT {
@@ -26,28 +24,19 @@ public class ManyToManyBiJIT {
     AuthorManyJ author2 = new AuthorManyJ();
     author2.setName("janko");
 
-    author1 = service.saveAuthor(author1);
-    author2 = service.saveAuthor(author2);
-
-   /*
-    service.saveBook(book);
-
     book.addAuthor(author1);
     book.addAuthor(author2);
 
+    //org.springframework.orm.jpa.JpaSystemException: Could not set field value [SHORT_CIRCUIT_INDICATOR]
+    // value by reflection :
+    // [class me.vukas.hiperfjavapersistence.entity.relationship.bidirectional.manytomanyjunction.BookAuthorId.authorId]
+    // setter of
+    // me.vukas.hiperfjavapersistence.entity.relationship.bidirectional.manytomanyjunction.BookAuthorId.authorId;
+    // nested exception is org.hibernate.PropertyAccessException: Could not set field value
+    // [SHORT_CIRCUIT_INDICATOR] value by reflection :
+    // [class me.vukas.hiperfjavapersistence.entity.relationship.bidirectional.manytomanyjunction.BookAuthorId.authorId]
+    // setter of
+    // me.vukas.hiperfjavapersistence.entity.relationship.bidirectional.manytomanyjunction.BookAuthorId.authorId
     service.saveBook(book);
-    */
-
-    service.addAuthorsToBook(book, author1, author2);
-
-    //remove author2 from book
-    //service.removeAuthorFromBook(book.getId(), author2.getId());
-
-    //remove author2 from book
-    service.removeAuthorInTrans(book, author2);
-
-    service.getBook(book.getId()).ifPresent(b -> {
-      assertThat(b.getId()).isEqualTo(1);
-    });
   }
 }
